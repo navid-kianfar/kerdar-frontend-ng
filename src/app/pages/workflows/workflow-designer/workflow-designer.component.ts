e7b7b7c170505d9e966ede5af4092041829f8db4
navidkianfar <t5bushVeJn2TDaFYmWZ7Xseb6XcQjqfkmFb5uFeUJF3wxUmJbgcLTXbVm3Zj83Lp>
2022-11-03 06:04:17 +0300

    favicons and pwa icons

--- src/app/pages/workflows/workflow-designer/workflow-designer.component.ts
+++ src/app/pages/workflows/workflow-designer/workflow-designer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WaitingLoaderInfo} from '../../../core/dtos/shared-dtos';
 
 @Component({
   selector: 'app-workflow-designer',
@@ -7,11 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkflowDesignerComponent implements OnInit {
   waiting: boolean = false;
+  waitingLoaderInfo: WaitingLoaderInfo = { title: '', current: '' };
 
   constructor() { }
 
   ngOnInit(): void {
     this.waiting = true;
-    setTimeout(() => this.waiting = false, 1000);
+    this.waitingLoaderInfo = {
+      title: 'Please wait, Workflow and related components are being loaded',
+      current: 'Loading contact module',
+      indeterminate: true,
+      percentage: 0,
+      description: '32 components, 13 events'
+    };
+    setTimeout(() => this.waiting = false, 10000);
   }
 }
